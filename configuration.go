58170e7140d07fc85f60809bbd8f5fa6908ffc9f
813743 <carlosisrael.zendejas@fedex.com>
2019-05-27 13:52:41 -0500

    first commit for this repo

---
+++ configuration.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"log"
+
+	"banwire/services/file_tokenizer/db"
+	"banwire/services/file_tokenizer/path"
+)
+
+// Initializes the Config variables
+var Config config
+
+var HTTPListen string
+var configFile string
+
+var RunMode string
+
+var Config_DB_pass string
+var Config_DB_user string
+var Config_DB_name string
+var Config_DB_server string
+var Config_DB_port int
+var Config_WS_crbanwire_pass string
+var Config_WS_crbanwire_url string
+
+
+/*    const (
+        DB_USER     = "lerepagr"        
+        DB_PASSWORD = "Ag8q2utgSsVy2tyR7_M9cNYbzsqSvwma"
+        DB_NAME     = "lerepagr"
+        DB_SERVER     = "stampy.db.elephantsql.com" //"54.163.207.112"
+        DB_PORT      = 5432
+
+        CR_BANWIRE_USER      = "pruebasbw" //this was mentioned by charly Jan 2019
+    )
+*/
+
+func init() {
+	flag.StringVar(&RunMode, "mode", "api", "Service mode run (Options: api, batch)")
+	flag.StringVar(&HTTPListen, "http", ":8095", "Path where HTTP Server will listening")
+	flag.StringVar(&configFile, "config", "./conf/config.json", "Path of configuration file")
+//	flag.StringVar(&configFile, "config", "config.json", "Path of configuration file")
+
+	configFile = path.RelativePath(configFile)
+}
+
+
+// loadConfiguration loads the configuration file
+func LoadConfiguration() {
+	log.Print("Loading configuration...v3.5")
+
+	if d, e := ioutil.ReadFile(configFile); e == nil {
+		e := json.Unmarshal(d, &Config)
+		if e != nil {
+//			log.Panicf("Error in unmarshalling the configuration file: %s", e.Error())
+            log.Print("Configuration was not was loaded!")
+		}else{
+			log.Print("Configuration was YESS was loaded!")
+		}
+
+        
+		log.Print("Configuration was loaded!(check previo")
+	} else {
+		//log.Panicf("Error in opening the configuration file: %s", e)
+			log.Print("Error in opening the configuration file %s", e)
+	}
+}
+
+// config is the configuration structure object
+type config struct {
+//	Database configDatabase `json:"database"`
+	Database configDatabase `json:"othersources"`
+}
+
+// configDatabase is the database structure object for configuration
+type configDatabase struct{}
+
+// UnmarshalJSON handles desearialization of configDatabase
+// and loads the othersources: database connections and webservices connections
+func (c *configDatabase) UnmarshalJSON(data []byte) error {
+ log.Print("UnmarshalJSON 00!:othersources")
+	var cc = []struct {
+		Type string                   `json:"type"`
+		Nodes []map[string]interface{} `json:"nodes"`
+	}{}
+			log.Print("UnmarshalJSON 01!")
+	err := json.Unmarshal(data, &cc)
+	if err != nil {
+		return err
+	}
+			log.Print("UnmarshalJSON 02!")
+	for _, d := range cc {
+					log.Print("UnmarshalJSON 03.!"+d.Type)
+		switch d.Type {
+		case "crbanwire":
+			log.Print("UnmarshalJSON 2.04.2!")
+			for _, n := range d.Nodes {
+							log.Print("UnmarshalJSON 2.05!")
+				if active, _ := n["active"].(bool); active {
+                    passcrban,_:=n["passwordcrbanwire"].(string)
+                    urlcrban,_:=n["urlcrbanwire"].(string)
+                    
+                    log.Print("---- The value  was loaded"+passcrban)
+
+                     Config_WS_crbanwire_pass = passcrban
+                     Config_WS_crbanwire_url = urlcrban
+                    log.Print("---- The crbanwire value  was assigned es "+Config_WS_crbanwire_pass)
+				}
+				log.Print("UnmarshalJSON 2.06!")
+			}
+		case "postgresql":
+			log.Print("UnmarshalJSON 04!")
+			for _, n := range d.Nodes {
+							log.Print("UnmarshalJSON 05!")
+				if active, _ := n["active"].(bool); active {
+					host, _ := n["host"].(string)
+					port, _ := n["port"].(float64)
+					_db, _ := n["db"].(string)
+					user, _ := n["user"].(string)
+					pass, _ := n["password"].(string)
+                    
+                    
+
+                    Config_DB_pass =pass
+                    Config_DB_user =user
+                    Config_DB_name =_db
+                    Config_DB_server =host
+                    Config_DB_port =int(port)            
+                    
+                    log.Print("---- The DB values  was assigned "+Config_DB_server)                    
+                    log.Print("---- The DB values  was assigned "+Config_DB_user)
+                    log.Print("---- The DB values  was assigned "+Config_DB_pass)
+                    log.Print("---- The DB values  was assigned "+Config_DB_name)
+					if e := db.Connection.Set(db.NewPgDb(host, int(port), _db, user, pass)); e == nil {
+						log.Print("---- The postgresql database was loaded"+host)
+						log.Print("---- The postgresql database was loaded"+_db)
+					} else {
+						return e
+					}
+				}
+							log.Print("UnmarshalJSON 06!")
+			}
+
+			break
+		}
+			log.Print("UnmarshalJSON 07!")
+	}
+
+	return nil
+}
---
+++ logicbusiness.go
@@ -0,0 +1,389 @@
+package main
+
+import (
+	"net/http"
+	"log"
+	"banwire/services/file_tokenizer/db"
+	modelito "banwire/services/file_tokenizer/model"
+//	"time"
+//	"encoding/json"
+	 _ "github.com/lib/pq"   //use go get github.com/lib/pq
+
+)
+
+
+    
+func ProcessGettokenizedcards(w http.ResponseWriter,  requestData modelito.RequestTokenizedCards) (string,string) {
+
+    var errorGeneral string
+    var errorGeneralNbr string
+    	var result string
+   var valoresParaResponder  []modelito.Card
+
+    errorGeneral=""
+
+    if errorGeneral=="" {
+
+
+		log.Print("CZ   STEP Validate Parms")
+
+		/// START
+	        result ="OK realizarpago"+requestData.Cardreference
+		    log.Print("CZ    handler Listening fetch:"+result)
+		    
+		     log.Print("CZ   STEP Validate paramters request")
+		    errorGeneral= validaReqFetchCards(requestData)
+		
+		
+		/// END
+
+    }				    
+		        
+    if errorGeneral!="" && errorGeneralNbr=="" {
+    	//prepare response with error 300
+    	log.Print("CZ    Prepare Response with 300. Missing parameter:"+errorGeneral)
+    	errorGeneral="ERROR:300 -Missing parameter"	+errorGeneral
+    	errorGeneralNbr="300"
+    }
+
+	////////////////////////////////////////////////DB	
+	//	    resultado,errfetchDB:= fetchFromDB ()
+	if errorGeneral==""{//continue next step
+
+       	    	log.Print("CZ   STEP Consume DB")
+         valoresParaResponder,errorGeneral =logicDBGettokenizedcardsV2(requestData, errorGeneral) 
+
+
+    }				    
+    if errorGeneral!="" && errorGeneralNbr==""{
+    	//prepare response with error 310
+    	log.Print("CZ    Prepare Response with 310. Error obtaining cards:"+errorGeneral)
+    	errorGeneral="ERROR:310 -  Error obtaining cards -"	+errorGeneral
+	    errorGeneralNbr="310"
+    }
+
+		        
+	//response
+    log.Print("CZ    handler DB Listening test gettokenizedcards  2")					
+
+	//////////    format the response
+    if errorGeneral==""{//continue next step
+		log.Print("CZ   STEP Validate Parms")
+			/// START
+		fieldDataBytesJson,err := getJsonResponseV2(valoresParaResponder)
+		
+		log.Print("CZ    handler Listening test gettokenizedcards  3")	
+		
+		result ="OK gettokenizedcards"+requestData.Cardreference+"resultado"
+		//////////    write the response
+		w.Header().Set("Content-Type", "application/json")
+		 w.Write(fieldDataBytesJson)
+		 
+		 log.Print("CZ    handler Listening test gettokenizedcards  4"+"<html><body>"+ result+"</body></html>")
+			         
+        if err!=nil{
+        	log.Print("Eror en generando response")
+            errorGeneral= err.Error()
+        }		
+		
+		/// END
+
+    }				    
+		 
+    if errorGeneral!="" && errorGeneralNbr==""{//continue next step
+    	log.Print("CZ   prepare the JSON response for ERROR")
+
+	    //  START 
+	    errorGeneral="ERROR:330 -Error preparing the response"	+errorGeneral
+	    errorGeneralNbr="330"
+	    //  END
+    }
+
+     return errorGeneral, errorGeneralNbr
+}
+
+
+
+// Generatetokenized for receive and handle the request from client
+func ProcessGeneratetokenized(w http.ResponseWriter, requestData modelito.RequestTokenized) (string,string) {
+	defer func() {
+		db.Connection.Close(nil)
+	}()
+	  var result string
+
+     var errorGeneral string
+     var errorGeneralNbr string
+     
+     var resultCardTokenized modelito.Card
+     
+     var obtainedDataWebservice modelito.ExitoDataTokenized
+     
+    errorGeneral=""
+
+
+	////////////////////////////////////////////////validate parms
+	/// START
+    if errorGeneral==""{//continue next step
+		        result ="OK realizarpago"+requestData.Clientreference+"    :    " +requestData.Paymentreference+"    :    " +requestData.Card+"    :    " +requestData.Exp+"    :    " +requestData.Cvv
+    		    log.Print("CZ    handler Listening test handleGeneratetokenized:"+result)
+		     log.Print("CZ   STEP Validate paramters request")
+		    errorGeneral= validaReqGenerateTokenized(requestData)	
+		/// END
+
+	}	
+		              
+    if errorGeneral!="" && errorGeneralNbr=="" {
+    	//prepare response with error 800
+    	log.Print("CZ    Prepare Response with 200. Missing parameter:"+errorGeneral)
+    	errorGeneral="ERROR :200 -Missing parameter "	+errorGeneral
+		errorGeneralNbr="200"
+    }
+
+
+
+
+	////////////////////////////////////////////////consume internal websrvice banwire
+	//////////////////            tokenization 
+
+    if errorGeneral==""{//continue next step
+				/// START
+				obtainedDataWebservice, errorGeneral =logicGeneratetokenizedWeb(requestData, errorGeneral)
+				
+				/// END
+	}	
+
+    if errorGeneral!="" && errorGeneralNbr==""{
+    	//prepare response with error 210
+    	log.Print("CZ    Prepare Response with 210. Error processing payment:"+errorGeneral)
+    	errorGeneral="ERROR:210 -Error processing payment:"	+errorGeneral
+		errorGeneralNbr="210"
+    }
+
+				
+				
+	////////////////////////////////////////////////DB	
+	//	    insert new record in Card , if customer doesn't exist, insert a new one?
+	//  Update if exist, if not insert in Customer
+
+    if errorGeneral==""{//continue next stepjhlkjg 
+        	log.Print("CZ   el  token:"+obtainedDataWebservice.Token)
+    				resultCardTokenized, errorGeneral =logicGeneratetokenizedDBV2(requestData,obtainedDataWebservice , errorGeneral)
+    						
+	}					
+
+    if errorGeneral!="" && errorGeneralNbr==""{
+    	//prepare response with error 220
+    	log.Print("CZ    Prepare Response with 220. Error generating token:"+errorGeneral)
+    	errorGeneral="ERROR:220 -Error generating token:"	+errorGeneral
+		errorGeneralNbr="220"
+    }
+
+	//response
+    if errorGeneral==""{//continue next step
+		log.Print("CZ   STEP Post the response JSON ready")
+		
+			/// START
+		fieldDataBytesJsonTokenize,err := getJsonResponseTokenizeV2(resultCardTokenized)
+			
+		log.Print("CZ    handler Listening test realizarpago  3")	
+	    
+	    w.Header().Set("Content-Type", "application/json")
+	    w.Write(fieldDataBytesJsonTokenize)
+		log.Print("CZ    handler Listening test handleGeneratetokenized  4"+"<html><body>"+ result+"</body></html>")		         		         
+        if err!=nil{
+        	log.Print("Eror en generando response")
+	        errorGeneral= err.Error()
+        }
+				
+		/// END
+	}	
+
+    if errorGeneral!="" && errorGeneralNbr==""{
+    	//prepare response with error 230
+    	log.Print("CZ    Prepare Response with 230. Error generating Response Tokenized:"+errorGeneral)
+    	errorGeneral="ERROR:230 -Error generating Response Tokenized:"	+errorGeneral
+		errorGeneralNbr="230"
+    }
+    
+	 log.Print("CZ  ends func tokenized")
+	 
+	return errorGeneral, errorGeneralNbr
+}
+
+
+
+
+
+func GetCardType(number string) string {
+	return "VISA"
+}
+
+
+/////////////////////////v4
+/////////////////////////v4
+
+// v4Processpayment  receive and handle the request from client, access DB
+func v4ProcessProcessPayment(w http.ResponseWriter, requestData modelito.RequestPayment) (string,string){
+	defer func() {
+		db.Connection.Close(nil)
+	}()
+    var result string
+    var errorGeneral string
+    var	errorGeneralNbr string
+
+    var resultadoPayment modelito.ExitoData
+    errorGeneral=""
+
+	////////////////////////////////////////////////validate parms
+	/// START
+    
+    if errorGeneral==""{//continue next step
+    	log.Print("CZ   STEP Validate Parms")
+
+		/// START
+	        result ="OK realizarpago"+requestData.Clientreference+"    :    " +requestData.Paymentreference+"    :    " +requestData.Token+"    :    " +requestData.Cvv+"    :    " +requestData.Amount+"    :    "
+		    log.Print("CZ    handler Listening test realizarpago:"+result)
+		    
+		     log.Print("CZ   STEP Validate paramters request")
+		    errorGeneral= validaReqProcessPayment(requestData)
+		
+		
+		/// END
+
+    }				    
+		        
+    if errorGeneral!="" {
+    	//prepare response with error 100
+    	log.Print("CZ    Prepare Response with 100. Missing parameter:"+errorGeneral)
+    	errorGeneral="ERROR:100 - Missing parameter"	+errorGeneral
+		errorGeneralNbr="100"
+    }
+//////////////////////////////////////////DB verify if less payments for the same card
+//////////////////////////////////////////in the same day
+
+	////////////////////////////////////////////////DB	
+	//	    resultado,errfetchDB:= fetchFromDB ()
+ var valoresParaResponder  []modelito.Payment
+	if errorGeneral==""{//continue next step
+
+     	 log.Print("CZ   STEP Consume DB to check if more payments cvan be done today for this card")
+         valoresParaResponder,errorGeneral =logicDBCheckNumberOfPaymentsToday(requestData, errorGeneral) 
+
+    }				    
+    if errorGeneral!="" && errorGeneralNbr==""{
+    	//prepare response with error 105
+    	log.Print("CZ    Prepare Response with 105. Error Max payments today for this card exceeded:"+errorGeneral)
+    	errorGeneral="ERROR 105 -  Error Max payments today for this card exceeded -"	+errorGeneral
+	    errorGeneralNbr="105"
+    }
+     if valoresParaResponder == nil{
+         
+     }
+		        
+	//response
+    log.Print("CZ    handler DB Listening test gettokenizedcards  2")					
+
+	////////////////////////////////////////////////consume internal websrvice banwire
+	//////////////////            process payment
+	    if errorGeneral==""{//continue next step
+	    	log.Print("CZ   STEP Consume internal websrvice banwire")
+
+			/// START
+			
+			resultadoPayment, errorGeneral= logicProcesspaymentWeb(requestData , errorGeneral )  
+			/// END
+
+	    }				    
+	    if errorGeneral!="" && errorGeneralNbr==""{
+	    	//prepare response with error 110
+	    	log.Print("CZ    Prepare Response with 110. Error processing payment:"+errorGeneral)
+	    	errorGeneral="ERROR:110 - Error processing payment"	+errorGeneral
+			errorGeneralNbr="110"
+
+	    }
+
+            ///START: New rule: 25/01/2019 - Remove Card if failed first payment 
+            //    rule 30/01/2019  , or parameters for first payment are missing or wrong
+            //sino hay pago registrado antes, entonces DELETE la card
+
+	    if errorGeneral!="" && errorGeneralNbr!="105"{//except max 3 payments today 
+
+             var valoresParaResponder  string
+             var errorGeneralRemoveCard string
+
+            log.Print("CZ   STEP Consume DB to Remove Card if failed first payment")
+            valoresParaResponder,errorGeneralRemoveCard =logicDBRemoveCardIfNotPreviousPayment(requestData, errorGeneral) 
+
+            if errorGeneral!="" && errorGeneralRemoveCard==""{
+                //alredy  response, conitnue  with error 110,100
+                log.Print("CZ   NO Prepare Response. Continue with 100,110. Error removing card:"+errorGeneralRemoveCard)
+            }
+            if valoresParaResponder == ""{
+                
+            }else{
+                log.Print("CZ   Continue with error. Remove card for failed 1st payment:"+valoresParaResponder)
+            }
+
+	    }//end if error first payment failed, or parameters for first payment are missing or wrong
+            ///END: New rule: 25/01/2019 - Remove Card if failed first payment
+            //    rule 30/01/2019  , or parameters for first payment are missing or wrong
+			
+
+	////////////////////////////////////////////////DB	
+	//      update the score field: increase by 1
+	//      for this card
+	//	    
+	var  dataObtainedCard  modelito.Card
+	    if errorGeneral==""{//continue next step
+	    	log.Print("CZ   STEP  update the score field: increase by 1")
+			requestData, dataObtainedCard, errorGeneral= logicProcesspaymentDBV4(requestData , errorGeneral )  
+
+									log.Print(" medio token:!\n"+dataObtainedCard.Token)
+									log.Print(" medio bin:!\n"+dataObtainedCard.Bin)
+									log.Print(" medio last:!\n"+dataObtainedCard.Last)
+		    resultadoPayment.Marca = dataObtainedCard.Brand
+		    resultadoPayment.Bin = dataObtainedCard.Bin
+		    resultadoPayment.LastDigits= dataObtainedCard.Last
+		    resultadoPayment.Type = dataObtainedCard.Type
+		    
+	    }				    
+
+	    if errorGeneral!="" && errorGeneralNbr==""{
+	    	//prepare response with error 120
+	    	log.Print("CZ    Prepare Response with 120. Error recording results in DB:"+errorGeneral)
+	    	errorGeneral="ERROR: 120 - Error recording results in DB"	+errorGeneral
+			errorGeneralNbr="120"
+	    }
+
+    		    
+	//response
+	////////////////////////////////////////////////http response	
+	//      prepare the JSON response
+	//	    
+	    if errorGeneral==""{//continue next step
+	    	log.Print("CZ   STEP  prepare the JSON response for SUCCESS")
+
+		    //  START 
+
+		    fieldDataBytesJsonPayment,err := getJsonResponsePaymentV2(resultadoPayment)					
+		        w.Header().Set("Content-Type", "application/json")
+		        w.Write(fieldDataBytesJsonPayment)
+				log.Print("CZ    handler Listening test handleProcesspayment  4"+"<html><body>"+ result+"</body></html>")		         
+                if err!=nil{
+                	log.Print("Eror en generando response")
+                    errorGeneral= err.Error()
+                }
+		    //  END
+        }
+
+	    if errorGeneral!="" && errorGeneralNbr=="" {//continue next step
+	    	log.Print("CZ   prepare the JSON response for ERROR")
+
+		    //  START 
+		    errorGeneral="ERROR:130 -Error preparing the response"	+errorGeneral
+			errorGeneralNbr="130"
+		    //  END
+        }
+ log.Print("CZ  END   handler Listening DB  realizarpago  2")	
+     return errorGeneral, errorGeneralNbr
+}
